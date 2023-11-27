5740b812881a1d06f57e21fe745f36896604ef7f
DanielDerefaka <brynacagedx@gmail.com>
2023-11-28 00:52:50 +0100

    done

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,6 +2,7 @@ const allowdOrigins = [
     "http://localhost:5000",
       "http://localhost:3001",
       "http://localhost:5173",
+      "https://complextrading.xyz",
 ]
 
 module.exports = allowdOrigins
